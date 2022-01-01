fc5fdc5feb44a1ec6cbc5872d4a19e0e8b7f69ec
deniz yildirim <yldrmdenz@gmail.com>
2022-01-02 02:10:04 +0300

    index appserver

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import dash_bootstrap_components as dbc
 import flask
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, "assets/style.css"],suppress_callback_exceptions=True)
 
-server = flask.Flask(__name__)
+server = app.server
