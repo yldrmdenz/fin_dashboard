6d7bcb805837656e9bc5b8e3d44cc2cf74ec8b34
deniz yildirim <yldrmdenz@gmail.com>
2022-01-03 18:29:07 +0300

    multi column news are generated

--- index.py
+++ index.py
@@ -16,11 +16,7 @@ import layouts
 import callbacks
 from app import app, server
 from jumbo_cols import jumbotron, second_jumbotron
-from news_api import  parse_rss_data
-
-
-
-
+from news_api import parse_rss_data
 app.layout = html.Div(
     children=[
         dbc.Navbar(
@@ -43,33 +39,89 @@ app.layout = html.Div(
         ),
         jumbotron,
         second_jumbotron,
-        dbc.ListGroup(children=[],id='news-list-group'),
+        dbc.Row(children=[
+            dbc.Col(dbc.ListGroupItem(id='yahoo-fin-cont')),
+            dbc.Col(dbc.ListGroupItem(id='wash-post-cont')),
+            dbc.Col(dbc.ListGroupItem(id='fin-times-cont')),
+        ], id='news-list-group'),
         html.Div(id='page-content')
     ]
 )
 
-@app.callback(Output('news-list-group','children'),
-              Input('interval-component','n_intervals'))
-def update_news(refresh):
+
+@app.callback([Output('yahoo-fin-cont', 'children'),
+               Output('wash-post-cont', 'children'),
+               Output('fin-times-cont', 'children')],
+              Input('interval-component', 'n_intervals'))
+def update_news(refresh) :
     news_dict = parse_rss_data()
     list_of_news = []
-    for value in news_dict.values() :
-        list_of_news.append(
-            dbc.ListGroupItem(
-                [
-                    html.Div(
-                        [
-                            html.H5(value['title'], className="mb-1"),
-                            html.Small("Yay!", className="text-success"),
-                        ],
-                        className="d-flex w-100 justify-content-between",
-                    ),
-                    html.P(value['body'], className="mb-1"),
-                    html.Small("Plus some small print.", className="text-muted"),
-                ]
-            )
-        )
-    return list_of_news
+    list_of_yf = []
+    list_of_wp = []
+    list_of_ft = []
+    for source in parse_rss_data() :
+        for new in source.values() :
+            if new['title'] == 'Yahoo Finance' :
+                list_of_yf.append(dbc.ListGroupItem(
+                    [
+                        html.Div(
+                            [
+                                html.H5(new['title'], className="mb-1"),
+                                html.Small("Breaking!", className="text-success"),
+                            ],
+                            className="d-flex w-100 justify-content-between",
+                        ),
+                        html.P(new['body'], className="mb-1"),
+                        html.Small("Plus some small print.", className="text-muted"),
+                    ]
+                ))
+            if new['title'] == 'Washington Post' :
+                list_of_wp.append(dbc.ListGroupItem(
+                    [
+                        html.Div(
+                            [
+                                html.H5(new['title'], className="mb-1"),
+                                html.Small("Breaking!", className="text-success"),
+                            ],
+                            className="d-flex w-100 justify-content-between",
+                        ),
+                        html.P(new['body'], className="mb-1"),
+                        html.Small("Plus some small print.", className="text-muted"),
+                    ]
+                ))
+            if new['title'] == 'Financial Times' :
+                list_of_ft.append(dbc.ListGroupItem(
+                    [
+                        html.Div(
+                            [
+                                html.H5(new['title'], className="mb-1"),
+                                html.Small("Breaking!", className="text-success"),
+                            ],
+                            className="d-flex w-100 justify-content-between",
+                        ),
+                        html.P(new['body'], className="mb-1"),
+                        html.Small("Plus some small print.", className="text-muted"),
+                    ]
+                ))
+    return list_of_yf, list_of_wp, list_of_ft
+    # for value in news_dict.values() :
+    #     list_of_news.append(
+    #         dbc.ListGroupItem(
+    #             [
+    #                 html.Div(
+    #                     [
+    #                         html.H5(value['title'], className="mb-1"),
+    #                         html.Small("Yay!", className="text-success"),
+    #                     ],
+    #                     className="d-flex w-100 justify-content-between",
+    #                 ),
+    #                 html.P(value['body'], className="mb-1"),
+    #                 html.Small("Plus some small print.", className="text-muted"),
+    #             ]
+    #         )
+    #     )
+    # return list_of_news
+
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -84,7 +136,6 @@ def page_changer(page) :
         return layouts.layout_assets
     else :
         return html.Div('')
-
 
 
 if __name__ == "__main__" :
--- news_api.py
+++ news_api.py
@@ -11,12 +11,23 @@ def parse_rss_data() :
                 'Financial Times' : 'https://www.ft.com/?format=rss'
                 }
     news_dict = {}
+    yahoo_fin_dict = {}
+    wash_post_dict = {}
+    fin_times_dict = {}
     for url_name,url in url_dict.items():
         url = requests.get(url)
         soup = BeautifulSoup(url.content, 'xml')
         titles = soup.find_all('title')
+
         for ctr, title in enumerate(titles[2:]) :
             keyword = str(url_name) + '_' + str(ctr)
-            news_dict[keyword] = {'title' : url_name, 'body' : title.text}
-    return news_dict
+            if url_name == 'Yahoo Finance':
+                yahoo_fin_dict[ctr] = {'title' : url_name, 'body' : title.text}
+            if url_name == 'Washington Post':
+                wash_post_dict[ctr] = {'title' : url_name, 'body' : title.text}
+            if url_name == 'Financial Times':
+                fin_times_dict[ctr] = {'title' : url_name, 'body' : title.text}
+
+            # news_dict[keyword] = {'title' : url_name, 'body' : title.text}
+    return yahoo_fin_dict,wash_post_dict,fin_times_dict
 
